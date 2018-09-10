gblomqvist/blender-Wirebomb

commit ae3992c33be92b4822c9ba3b425b78e149147077

Fix 1 bug which occurred when an interface language other than english was used

b_scene.py
@@ -148,22 +148,16 @@ class BlenderScene:
             # get layer n's stats
             layer = scene.layers[n]
             self.set_layers((layer,))
-            stats = scene.statistics()
-
-            # get layer n's tris count from its stats
-            geo_start = stats.index('Tris:') + 5
-            geo_end = stats.index(' ', geo_start)
-            geo_str = stats[geo_start:geo_end].split(',')
-            geo_int = int(''.join(geo_str))
+            tris_count = int(scene.statistics().split('|')[3].split(':')[1])
 
             if wanted == 'LEAST':
-                if wanted_geo is None or geo_int < wanted_geo:
-                    wanted_geo = geo_int
+                if wanted_geo is None or tris_count < wanted_geo:
+                    wanted_geo = tris_count
                     wanted_n = n
 
             elif wanted == 'MOST':
-                if wanted_geo is None or geo_int > wanted_geo:
-                    wanted_geo = geo_int
+                if wanted_geo is None or tris_count > wanted_geo:
+                    wanted_geo = tris_count
                     wanted_n = n
 
             else:

commit 2041c78d615af85fa88b9b2d0d9ef489e9391275

Fixed bug introduced by ae3992c

b_scene.py
@@ -125,48 +125,6 @@ class BlenderScene:
             for n in layers:
                 scene.layers[n] = True
 
-    def find_layer_by_geometry(self, wanted):
-        """Finds and returns the layer containing least/most geometry depending on wanted variable.
-
-        If multiple layers are correct and contain the same amount of geometry, the first one found will be returned.
-
-        Args:
-            wanted: A string, either 'LEAST' or 'MOST' depending on if you want the layer with the least/most geometry.
-
-        Returns:
-            The layer containing least/most geometry.
-        """
-        scene = self.set_as_active()
-        previous_layers = tuple(scene.layers)
-
-        # geometry and layer currently closest to meeting the wanted variable condition
-        wanted_geo = None
-        wanted_n = 0
-
-        for n in range(1, 20):
-
-            # get layer n's stats
-            layer = scene.layers[n]
-            self.set_layers((layer,))
-            tris_count = int(scene.statistics().split('|')[3].split(':')[1])
-
-            if wanted == 'LEAST':
-                if wanted_geo is None or tris_count < wanted_geo:
-                    wanted_geo = tris_count
-                    wanted_n = n
-
-            elif wanted == 'MOST':
-                if wanted_geo is None or tris_count > wanted_geo:
-                    wanted_geo = tris_count
-                    wanted_n = n
-
-            else:
-                raise ValueError("No such wanted as {}".format(wanted))
-
-        scene.layers = previous_layers
-
-        return wanted_n
-
     def get_layers_vert_counts(self):
         """Returns a list of integers where element i represents the number of vertices on layer i.
 

w_b_scene.py
@@ -59,7 +59,7 @@ class BlenderSceneW(BlenderScene):
         if not revert:
             scene.render.use_simplify = True
             scene.render.simplify_subdivision = 0
-            smallest_layer = self.find_layer_by_geometry('LEAST')
+            smallest_layer, _ = min(enumerate(self.get_layers_vert_counts()), key=lambda p: p[1])
             self.set_layers((smallest_layer,))
 
         else: